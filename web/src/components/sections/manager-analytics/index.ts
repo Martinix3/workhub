5fa5fbdbb9752055afdf3efb5c5ae83abdfff270
Martín Jaime Sampériz <martinjaimesamperiz@MacBook-Air.local>
2026-01-10 22:23:20 +0100

    auto-claude: 3.6 - Create index.ts file to export all manager analyti

---
+++ web/src/components/sections/manager-analytics/index.ts
@@ -0,0 +1,8 @@
+// Manager Analytics Components
+export { WorkloadDistributionChart } from './WorkloadDistributionChart'
+export { VelocityTrendChart } from './VelocityTrendChart'
+export { BlockerAnalysisPanel } from './BlockerAnalysisPanel'
+export { OverdueRatioChart } from './OverdueRatioChart'
+
+// Manager Analytics Types
+export type * from './types'
