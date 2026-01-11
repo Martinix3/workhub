7e42a0074b75aae1bf9056b29b1774fa55a53f7c
Martín Jaime Sampériz <martinjaimesamperiz@MacBook-Air.local>
2026-01-11 08:20:45 +0100

    auto-claude: 4.5 - Export all KPI builder components from index file

---
+++ web/src/components/kpi-builder/index.ts
@@ -0,0 +1,5 @@
+// KPI Builder Components
+export { MetricSelector } from './MetricSelector'
+export { ThresholdEditor } from './ThresholdEditor'
+export { VisualizationPicker } from './VisualizationPicker'
+export { KPIBuilderModal } from './KPIBuilderModal'
